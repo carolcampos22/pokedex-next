4c9187b1e6320f5509c76aeaa52c6155a8bc19ef
carolcampos22 <mendes.carolina2209@gmail.com>
2023-07-25 16:22:13 -0300

    Estilização do header e do footer

---
+++ app/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+    return (
+        <>
+            <link rel='shortcut icon' href='/images/favicon.ico' />
+            <title>Pokedéx</title>
+        </>
+    )
+}
